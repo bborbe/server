510cd5ff0f79ab2fed6b9bc05ab6c17e5c3eeac9
Benjamin Borbe <bborbe@rocketnews.de>
2013-08-05 20:23:28 +0200

    add google analytics renderer

---
+++ renderer/googleanalytics/googleanalytics_renderer.go
@@ -0,0 +1,32 @@
+package googleanalytics
+
+import (
+	"github.com/bborbe/server/renderer"
+	"github.com/bborbe/server/renderer/content"
+	"io"
+)
+
+const CONTENT string = `<script language="javascript" type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-325476-3']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>`
+
+type googleanalyticsRenderer struct {
+	renderer renderer.Renderer
+}
+
+func NewGoogleanalyticsRenderer() *googleanalyticsRenderer {
+	v := new(googleanalyticsRenderer)
+	v.renderer = content.NewContentRenderer(CONTENT)
+	return v
+}
+
+func (v *googleanalyticsRenderer) Render(writer io.Writer) error {
+	return v.renderer.Render(writer)
+}
---
+++ renderer/googleanalytics/googleanalytics_renderer_test.go
@@ -0,0 +1,31 @@
+package googleanalytics
+
+import (
+	. "github.com/bborbe/assert"
+	"github.com/bborbe/server/mock"
+	"github.com/bborbe/server/renderer"
+	"testing"
+)
+
+func TestImplementsRequestHandler(t *testing.T) {
+	v := NewGoogleanalyticsRenderer()
+	var i (*renderer.Renderer) = nil
+	err := AssertThat(v, Implements(i).Message("check implements view.Renderer"))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRender(t *testing.T) {
+	var err error
+	v := NewGoogleanalyticsRenderer()
+	writer := mock.NewWriter()
+	err = v.Render(writer)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = AssertThat(len(writer.Content()), Gt(0))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
