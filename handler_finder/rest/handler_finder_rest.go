c21293c489513005ebe36e3c70f32688a76c9c10
Benjamin Borbe <bborbe@rocketnews.de>
2015-01-02 14:15:28 +0100

    improve rest handlerfinder

--- handler_finder/rest/handler_finder_rest.go
+++ handler_finder/rest/handler_finder_rest.go
@@ -6,25 +6,31 @@ import (
 	"github.com/bborbe/server/handler/fallback"
 	"github.com/bborbe/server/handler_finder/method"
 	"github.com/bborbe/server/handler_finder/part"
+	"github.com/bborbe/server/handler_finder"
 )
 
 type restHandlerFinder struct {
-	handler http.Handler
+	handlerFinder handler_finder.HandlerFinder
 }
 
 func New(prefix string, listHandler http.Handler, getHandler http.Handler, createHandler http.Handler, updateHandler http.Handler, patchHandler http.Handler, deleteHandler http.Handler) *restHandlerFinder {
-	h := new(restHandlerFinder)
+
+	partHandlerFinder := part.New(prefix)
+	partHandlerFinder.RegisterHandler("/", getHandler)
+
 	methodHandlerFinder := method.New()
+	methodHandlerFinder.RegisterHandler("GET", fallback.NewFallback(partHandlerFinder, listHandler))
 	methodHandlerFinder.RegisterHandler("POST", createHandler)
 	methodHandlerFinder.RegisterHandler("PUT", updateHandler)
 	methodHandlerFinder.RegisterHandler("PATCH", patchHandler)
 	methodHandlerFinder.RegisterHandler("DELETE", deleteHandler)
-	partHandlerFinder := part.New(prefix)
-	partHandlerFinder.RegisterHandler("/", getHandler)
-	h.handler = fallback.NewFallback(methodHandlerFinder, fallback.NewFallback(partHandlerFinder, listHandler))
+
+
+	h := new(restHandlerFinder)
+	h.handlerFinder = methodHandlerFinder
 	return h
 }
 
 func (h *restHandlerFinder) FindHandler(request *http.Request) http.Handler {
-	return h.handler
+	return h.handlerFinder.FindHandler(request)
 }
--- handler_finder/rest/handler_finder_rest_test.go
+++ handler_finder/rest/handler_finder_rest_test.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/bborbe/assert"
 	"github.com/bborbe/server/handler/static"
 	"github.com/bborbe/server/handler_finder"
+	"github.com/bborbe/server/mock"
 )
 
 func TestImplementsHandlerFinder(t *testing.T) {
@@ -25,6 +26,28 @@ func TestImplementsHandlerFinder(t *testing.T) {
 	}
 }
 
+func TestGet(t *testing.T) {
+	listHandler := static.NewHandlerStaticContent("list")
+	getHandler := static.NewHandlerStaticContent("get")
+	createHandler := static.NewHandlerStaticContent("create")
+	updateHandler := static.NewHandlerStaticContent("update")
+	patchHandler := static.NewHandlerStaticContent("patch")
+	deleteHandler := static.NewHandlerStaticContent("delete")
+	hf := New("/test", listHandler, getHandler, createHandler, updateHandler, patchHandler, deleteHandler)
+	r := &http.Request{Method:"GET", RequestURI: "/test/1"}
+	h := hf.FindHandler(r)
+	err := AssertThat(h, NotNilValue())
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp := mock.NewHttpResponseWriterMock()
+	h.ServeHTTP(resp, r)
+	err = AssertThat(string(resp.Content()), Is("get"))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestList(t *testing.T) {
 	listHandler := static.NewHandlerStaticContent("list")
 	getHandler := static.NewHandlerStaticContent("get")
@@ -32,7 +55,105 @@ func TestList(t *testing.T) {
 	updateHandler := static.NewHandlerStaticContent("update")
 	patchHandler := static.NewHandlerStaticContent("patch")
 	deleteHandler := static.NewHandlerStaticContent("delete")
-	h := New("/test", listHandler, getHandler, createHandler, updateHandler, patchHandler, deleteHandler)
-	r := &http.Request{}
-	h.FindHandler(r)
+	hf := New("/test", listHandler, getHandler, createHandler, updateHandler, patchHandler, deleteHandler)
+	r := &http.Request{Method:"GET", RequestURI: "/test"}
+	h := hf.FindHandler(r)
+	err := AssertThat(h, NotNilValue())
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp := mock.NewHttpResponseWriterMock()
+	h.ServeHTTP(resp, r)
+	err = AssertThat(string(resp.Content()), Is("list"))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCreate(t *testing.T) {
+	listHandler := static.NewHandlerStaticContent("list")
+	getHandler := static.NewHandlerStaticContent("get")
+	createHandler := static.NewHandlerStaticContent("create")
+	updateHandler := static.NewHandlerStaticContent("update")
+	patchHandler := static.NewHandlerStaticContent("patch")
+	deleteHandler := static.NewHandlerStaticContent("delete")
+	hf := New("/test", listHandler, getHandler, createHandler, updateHandler, patchHandler, deleteHandler)
+	r := &http.Request{Method:"POST", RequestURI: "/test"}
+	h := hf.FindHandler(r)
+	err := AssertThat(h, NotNilValue())
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp := mock.NewHttpResponseWriterMock()
+	h.ServeHTTP(resp, r)
+	err = AssertThat(string(resp.Content()), Is("create"))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	listHandler := static.NewHandlerStaticContent("list")
+	getHandler := static.NewHandlerStaticContent("get")
+	createHandler := static.NewHandlerStaticContent("create")
+	updateHandler := static.NewHandlerStaticContent("update")
+	patchHandler := static.NewHandlerStaticContent("patch")
+	deleteHandler := static.NewHandlerStaticContent("delete")
+	hf := New("/test", listHandler, getHandler, createHandler, updateHandler, patchHandler, deleteHandler)
+	r := &http.Request{Method:"PUT", RequestURI: "/test"}
+	h := hf.FindHandler(r)
+	err := AssertThat(h, NotNilValue())
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp := mock.NewHttpResponseWriterMock()
+	h.ServeHTTP(resp, r)
+	err = AssertThat(string(resp.Content()), Is("update"))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPatch(t *testing.T) {
+	listHandler := static.NewHandlerStaticContent("list")
+	getHandler := static.NewHandlerStaticContent("get")
+	createHandler := static.NewHandlerStaticContent("create")
+	updateHandler := static.NewHandlerStaticContent("update")
+	patchHandler := static.NewHandlerStaticContent("patch")
+	deleteHandler := static.NewHandlerStaticContent("delete")
+	hf := New("/test", listHandler, getHandler, createHandler, updateHandler, patchHandler, deleteHandler)
+	r := &http.Request{Method:"PATCH", RequestURI: "/test"}
+	h := hf.FindHandler(r)
+	err := AssertThat(h, NotNilValue())
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp := mock.NewHttpResponseWriterMock()
+	h.ServeHTTP(resp, r)
+	err = AssertThat(string(resp.Content()), Is("patch"))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	listHandler := static.NewHandlerStaticContent("list")
+	getHandler := static.NewHandlerStaticContent("get")
+	createHandler := static.NewHandlerStaticContent("create")
+	updateHandler := static.NewHandlerStaticContent("update")
+	patchHandler := static.NewHandlerStaticContent("patch")
+	deleteHandler := static.NewHandlerStaticContent("delete")
+	hf := New("/test", listHandler, getHandler, createHandler, updateHandler, patchHandler, deleteHandler)
+	r := &http.Request{Method:"DELETE", RequestURI: "/test"}
+	h := hf.FindHandler(r)
+	err := AssertThat(h, NotNilValue())
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp := mock.NewHttpResponseWriterMock()
+	h.ServeHTTP(resp, r)
+	err = AssertThat(string(resp.Content()), Is("delete"))
+	if err != nil {
+		t.Fatal(err)
+	}
 }
