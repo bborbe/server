624dcfe5fe154892406cc678318af87b276d48aa
Benjamin Borbe <bborbe@rocketnews.de>
2015-08-02 21:35:03 +0200

    test json handler

--- handler/json/json_handler_test.go
+++ handler/json/json_handler_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	. "github.com/bborbe/assert"
+	"github.com/bborbe/server/mock"
 )
 
 func TestImplementsHandler(t *testing.T) {
@@ -12,6 +13,62 @@ func TestImplementsHandler(t *testing.T) {
 	var i *http.Handler
 	err := AssertThat(r, Implements(i))
 	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+type user struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+func TestRenderStruct(t *testing.T) {
+	u := &user{
+		FirstName: "Hello",
+		LastName:  "World",
+	}
+	r := NewJsonHandler(u)
+	resp := mock.NewHttpResponseWriterMock()
+	req, err := mock.NewHttpRequestMock("/")
+	if err = AssertThat(err, NilValue()); err != nil {
+		t.Fatal()
+	}
+	r.ServeHTTP(resp, req)
+	if err = AssertThat(string(resp.Content()), Is(`{"firstname":"Hello","lastname":"World"}`)); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRenderListOfStruct(t *testing.T) {
+	list := []user{user{
+		FirstName: "Hello",
+		LastName:  "World",
+	}, user{
+		FirstName: "Hello",
+		LastName:  "World",
+	}}
+	r := NewJsonHandler(list)
+	resp := mock.NewHttpResponseWriterMock()
+	req, err := mock.NewHttpRequestMock("/")
+	if err = AssertThat(err, NilValue()); err != nil {
+		t.Fatal()
+	}
+	r.ServeHTTP(resp, req)
+	if err = AssertThat(string(resp.Content()), Is(`[{"firstname":"Hello","lastname":"World"},{"firstname":"Hello","lastname":"World"}]`)); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRenderEmptyList(t *testing.T) {
+	list := make([]user, 0)
+	r := NewJsonHandler(list)
+	resp := mock.NewHttpResponseWriterMock()
+	req, err := mock.NewHttpRequestMock("/")
+	if err = AssertThat(err, NilValue()); err != nil {
+		t.Fatal()
+	}
+	r.ServeHTTP(resp, req)
+	if err = AssertThat(string(resp.Content()), Is(`[]`)); err != nil {
 		t.Fatal(err)
 	}
 }
