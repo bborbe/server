2218fd52d841bc180d749448605a39a44c842832
Benjamin Borbe <bborbe@rocketnews.de>
2016-05-18 21:10:58 +0200

    refactor

--- handler/debug/debug_handler.go
+++ handler/debug/debug_handler.go
@@ -9,21 +9,21 @@ import (
 
 var logger = log.DefaultLogger
 
-type logHandler struct {
-	handler http.Handler
+type handler struct {
+	subhandler http.Handler
 }
 
-func New(handler http.Handler) *logHandler {
-	m := new(logHandler)
-	m.handler = handler
+func New(subhandler http.Handler) *handler {
+	m := new(handler)
+	m.subhandler = subhandler
 	return m
 }
 
-func (m *logHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+func (m *handler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
 	start := time.Now()
 	defer logger.Debugf("%s %s takes %dms", request.Method, request.RequestURI, time.Now().Sub(start)/time.Millisecond)
 
 	logger.Debugf("request %v: ", request)
-	m.handler.ServeHTTP(responseWriter, request)
+	m.subhandler.ServeHTTP(responseWriter, request)
 	logger.Debugf("response %v: ", responseWriter)
 }
