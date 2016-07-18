5ef017d08be06d7e8d7a20ed7ed1e84281bc0c88
Benjamin Borbe <bborbe@rocketnews.de>
2016-07-18 22:09:19 +0200

    add redirect handler

---
+++ handler/redirect/redirect.go
@@ -0,0 +1,26 @@
+package redirect
+
+import (
+	"net/http"
+
+	"github.com/bborbe/log"
+)
+
+var logger = log.DefaultLogger
+
+type handler struct {
+	target string
+	status int
+}
+
+func New(target string) *handler {
+	h := new(handler)
+	h.target = target
+	h.status = http.StatusMovedPermanently
+	return h
+}
+
+func (h *handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	logger.Debugf("redirect to %s %d found %s %s", h.target, h.status)
+	http.Redirect(resp, req, h.target, h.status)
+}
